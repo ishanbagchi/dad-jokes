a3c46ca482b83b4702d7d1bea08c54790eaa2323
ishanbagchi <ishanbagchi4@gmail.com>
2022-03-31 10:40:58 +0530

    add alt txt in img tag

--- src/JokeList.js
+++ src/JokeList.js
@@ -91,7 +91,10 @@ export class JokeList extends Component {
 					<h1 className="JokeList-title">
 						<span>Dad</span> Jokes
 					</h1>
-					<img src="https://assets.dryicons.com/uploads/icon/svg/8927/0eb14c71-38f2-433a-bfc8-23d9c99b3647.svg" />
+					<img
+						src="https://assets.dryicons.com/uploads/icon/svg/8927/0eb14c71-38f2-433a-bfc8-23d9c99b3647.svg"
+						alt="Laughing Emoji"
+					/>
 					<button
 						className="JokeList-getmore"
 						onClick={this.handleClick}
